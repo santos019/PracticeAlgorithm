25d16f148f42a0752097bd52b0abe7adcc16ccd5
santos019 <santos019@naver.com>
2021-12-02 20:38:26 +0900

    [PRO,java]주식가격

---
+++ Java_Programers_주식가격.java
@@ -0,0 +1,20 @@
+//https://programmers.co.kr/learn/courses/30/lessons/42584
+//주식가격
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer= new int[prices.length];
+        int cnt=0;
+        for(int i=0; i<prices.length;i++)
+        {
+            for(int j=i+1; j<prices.length;j++)
+            {
+                cnt++;
+                if(prices[i]>prices[j])
+                    break;
+            }
+            answer[i]=cnt;
+            cnt=0;                        
+        }
+        return answer;
+    }
+}
