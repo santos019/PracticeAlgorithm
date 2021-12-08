add24c84ea2933af93102d0058582166c13c4484
santos019 <santos019@naver.com>
2021-12-08 23:10:36 +0900

    [Java]순열구현 시간초과

--- Java_Programers_가장큰수.java
+++ Java_Programers_가장큰수.java
@@ -1,39 +1,35 @@
+import java.util.ArrayList;
+import java.util.Collections;
 class Solution {
+    
+public static void comb(int[] numbers,int[] out,boolean[] check, ArrayList<String> myarray,int depth, int n, int r)
+    {
+     if(depth==r)
+     {  String str="";
+         for(int k=0;k<n;k++)
+            str=str+Integer.toString(out[k]);
+        myarray.add(str);
+      return;
+      }
+    for(int i=0;i<n;i++)
+    {
+        if(check[i]!=true)
+        {
+            check[i]=true;
+            out[depth]=numbers[i];
+            comb(numbers,out,check,myarray,depth+1,n,r);
+            check[i]=false;
+        }
+     }
+    }
+    
+    
     public String solution(int[] numbers) {
-        String answer = "";
-        String number="";
-        String number1="";
-        int[] my=new int[numbers.length];
-        int con=0;
-        char[] digits1;
-        for(int i=0;i<numbers.length;i++)
-        {
-         number = String.valueOf(numbers[i]);
-         digits1 = number.toCharArray();
-         my[i]= Character.getNumericValue(digits1[0]);
-       
-        }
-         for(int i=0;i<numbers.length;i++)
-         {
-             for(int j=i+1;j<numbers.length;j++)
-             {
-                 if(my[i]<my[j])
-                 {
-                     con=numbers[j];
-                     numbers[j]=numbers[i];
-                     numbers[i]=con;
-                     con=my[j];
-                     my[j]=my[i];
-                     my[i]=con;
-                 }
-             }
-         }
-        for(int k=0;k<numbers.length;k++)
-        {
-             number1 = String.valueOf(numbers[k]);
-            answer=answer+number1;
-        }
-
-        return answer;
+        int[] out= new int[numbers.length];
+        boolean[] check= new boolean[numbers.length];
+        ArrayList <String> myarray =new ArrayList<>();      
+        comb(numbers,out,check,myarray,0,numbers.length,numbers.length);
+        Collections.sort(myarray,Collections.reverseOrder()); 
+        return  Integer.parseInt(myarray.get(0))/Math.pow(10,myarray.size()-1)==0?"0":myarray.get(0);
     }
 }
