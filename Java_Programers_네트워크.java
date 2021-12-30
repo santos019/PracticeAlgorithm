f10aecc4ad18dfc66e0f140e6a398407988fb3b9
santos019 <santos019@naver.com>
2021-12-30 22:35:25 +0900

    [PRO,Java]네트워크

---
+++ Java_Programers_네트워크.java
@@ -0,0 +1,35 @@
+class Solution {
+  static int cnt=0;
+    public int solution(int n, int[][] computers) {
+        //System.out.println(computers.length);
+        int answer = 0;
+         boolean[] visited =new boolean[computers.length];
+       
+        for(int k=0;k<computers.length;k++)
+        {   if(visited[k]!=true)       
+            {
+            dfs(k,visited,computers); 
+            cnt++;
+            }
+          
+        }
+        //System.out.println("cnt"+cnt);
+        return cnt;
+    }
+
+static void dfs(int node, boolean[] visited,int[][] computers)
+{   //if(visited[node]==true)return;
+    visited[node]=true;
+    //System.out.println(node+" ");
+    
+    for(int next=0;next<computers.length;++next)
+    {
+        if(!visited[next] && computers[node][next]!=0)
+        dfs(next,visited,computers);
+     
+        
+    }
+    
+}
+
+}
